1fd7f08d135c8982f2dceccc01528e71a3741821
code-art4 <ojotriumph4@gmail.com>
2022-03-11 01:57:17 +0100

    did the functionality of the fourth step form

--- pages/dashboard/products/FormComponents/FormComponents.js
+++ pages/dashboard/products/FormComponents/FormComponents.js
@@ -298,7 +298,7 @@ const SecondComponent = () => {
   );
 };
 
-const ThirdComponent = () => {
+const ThirdComponent = () => { 
   return (
     <>
       <h1 className='text-4xl font-semibold text-grey_20'>Product Content</h1>
@@ -313,9 +313,9 @@ const ThirdComponent = () => {
               <p className='mt-2 text-grey_60'>
                 Upload your product files here
               </p>
-              <p className='flex w-[max-content] mx-auto border border-solid border-black p-3 mt-3 rounded-xl cursor-pointer'>
+              <span className='flex w-[max-content] mx-auto border border-solid border-black p-3 mt-3 rounded-xl cursor-pointer'>
                 Upload Files <p className='ml-2 text-grey_40'>+</p>
-              </p>
+              </span>
             </label>
           </div>
 
@@ -343,6 +343,13 @@ const ThirdComponent = () => {
 };
 
 const FourthComponent = () => {
+  const [productPricingSettings, setProductPricingSettings] = useState(false);
+
+  const handleTogglePricingSettings = () => {
+    setProductPricingSettings(prev => !prev);
+  };
+
+  console.log(productPricingSettings);
   return (
     <>
       <h1 className='text-4xl font-semibold text-grey_20'>Product Pricing</h1>
@@ -376,9 +383,61 @@ const FourthComponent = () => {
                     Let customers pay what they want?
                   </p>
                 </div>
-                <Toggle label='TogglePayment' />
+                <Toggle
+                  label='TogglePayment'
+                  click={handleTogglePricingSettings}
+                />
               </div>
             </div>
+            {productPricingSettings && <div className='flex flex-col mt-5 rounded-xl   border border-solid border-grey_80 p-4'>
+              {productPricingSettings ? (
+                <div className='flex justify-between items-center mb-3'>
+                  <div className='flex flex-col items-between w-[45%]'>
+                    <div className='flex flex-col mt-5'>
+                      <label htmlFor='product_name' className='text-lg'>
+                        Minimum Amount{' '}
+                      </label>
+                      <div className='flex items-center mt-2 justify-between rounded-xl border border-solid border-grey_85'>
+                        <input
+                          className='px-4 rounded-xl text-grey_20 p-3 w-[4rem] border-r-0 rounded z-0 bg-grey_95 text-center outline-none'
+                          value='$'
+                          readOnly
+                        />
+                        <input
+                          type='text'
+                          name='Amount'
+                          id='Amount'
+                          placeholder='0+'
+                          className=' outline-none w-[98%] p-3 rounded-xl z-10'
+                        />
+                      </div>
+                    </div>
+                  </div>
+
+                  <div className='flex flex-col items-between w-[45%]'>
+                    <div className='flex flex-col mt-5'>
+                      <label htmlFor='product_name' className='text-lg'>
+                        Maximum Amount{' '}
+                      </label>
+                      <div className='flex items-center mt-2 justify-between rounded-xl border border-solid border-grey_85'>
+                        <input
+                          className='px-4 rounded-xl text-grey_20 p-3 w-[4rem] border-r-0 rounded z-0 bg-grey_95 text-center outline-none'
+                          value='$'
+                          readOnly
+                        />
+                        <input
+                          type='text'
+                          name='Amount'
+                          id='Amount'
+                          placeholder='0+'
+                          className=' outline-none w-[98%] p-3 rounded-xl z-10'
+                        />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              ) : null}
+            </div>}
           </div>
         </div>
       </div>
