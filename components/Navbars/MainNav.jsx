6562860102c91fad928910e77151f5d50471d899
umarmuhd <umarfarouqft@gmail.com>
2022-03-24 11:53:09 +0100

    Merge branch 'main' of https://github.com/Umarmuhd/cabiza-fe-v1 into main

--- components/Navbars/MainNav.jsx
+++ components/Navbars/MainNav.jsx
@@ -32,89 +32,226 @@ const NavIcon = () => (
 );
 
 const RightIcon = () => (
-  <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg" className="ml-3">
-  <path d="M1 8H15M15 8L7.99998 0.999878M15 8L7.99998 14.9999" stroke="#5B44E9" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+  <svg
+    width="16"
+    height="16"
+    viewBox="0 0 16 16"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    className="ml-3"
+  >
+    <path
+      d="M1 8H15M15 8L7.99998 0.999878M15 8L7.99998 14.9999"
+      stroke="#5B44E9"
+      stroke-width="1.5"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
   </svg>
-)
+);
 
 export default function MainNavigation() {
-  const [navbarOpen, setNavbarOpen] = React.useState(false);
+  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
   return (
-    <>
-      <nav className='relative flex flex-wrap items-center justify-between px-0 md:px-4 py-3'>
-        <div className='container px-4 mx-auto flex flex-wrap items-center justify-between'>
-          <div className='w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start'>
-            <Link href='/'>
-              <a className='text-white text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase'>
-                <img src='/images/logo.png' alt='...' />
+    <div className="px-4 py-5 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
+      <div className="relative flex items-center justify-between">
+        <a
+          aria-label="Cabiza"
+          title="Cabiza"
+          className="inline-flex items-center"
+        >
+          <svg
+            className="w-8 text-deep-purple-accent-400"
+            viewBox="0 0 24 24"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeMiterlimit="10"
+            stroke="currentColor"
+            fill="none"
+          >
+            <rect x="3" y="1" width="7" height="12" />
+            <rect x="3" y="17" width="7" height="6" />
+            <rect x="14" y="1" width="7" height="6" />
+            <rect x="14" y="11" width="7" height="12" />
+          </svg>
+          <span className="ml-2 text-xl font-bold tracking-wide text-gray-800 uppercase">
+            Cabiza
+          </span>
+        </a>
+        <ul className="flex items-center hidden space-x-8 lg:flex">
+          <li>
+            <a
+              aria-label="Our product"
+              title="Our product"
+              className="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
+            >
+              Home
+            </a>
+          </li>
+          <li>
+            <a
+              aria-label="Our product"
+              title="Our product"
+              className="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
+            >
+              Discover
+            </a>
+          </li>
+          <li>
+            <a
+              aria-label="Product pricing"
+              title="Product pricing"
+              className="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
+            >
+              Membership
+            </a>
+          </li>
+        </ul>
+        <ul className="flex items-center hidden space-x-8 lg:flex">
+          <li className="flex items-center">
+            <Link href="/auth/login">
+              <a
+                className="font-medium md:px-4 py-2 hover:font-semibold focus:outline-none lg:mr-1 lg:mb-0 ml-3 mb-3"
+                style={{ transition: "all .15s ease" }}
+              >
+                Login
               </a>
             </Link>
-            <button
-              className='cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none'
-              type='button'
-              onClick={() => setNavbarOpen(!navbarOpen)}
-            >
-              <NavIcon />
-            </button>
-          </div>
-          <div
-            className={
-              'lg:flex flex-grow items-center bg-primary lg:bg-transparent lg:shadow-none' +
-              (navbarOpen ? ' block rounded shadow-lg' : ' hidden')
-            }
-            id='example-navbar-warning'
+          </li>
+          <li>
+            <Link href="/auth/signup">
+              <a
+                className="border text-primary border-primary font-semibold px-4 py-2 rounded-xl shadow hover:shadow-md outline-none focus:outline-none lg:mr-1 lg:mb-0 md:ml-3 mb-3 flex items-center"
+                style={{ transition: "all .15s ease" }}
+              >
+                Sign up <RightIcon />
+              </a>
+            </Link>
+          </li>
+        </ul>
+        <div className="lg:hidden">
+          <button
+            aria-label="Open Menu"
+            title="Open Menu"
+            className="p-2 -mr-1 transition duration-200 rounded focus:outline-none focus:shadow-outline hover:bg-deep-purple-50 focus:bg-deep-purple-50"
+            onClick={() => setIsMenuOpen(true)}
           >
-            <ul className='flex flex-col lg:flex-row list-none lg:ml-auto'>
-              <li className='flex items-center'>
-                <Link href='/'>
-                  <a className='px-3 py-4 lg:py-2 flex items-center text-base text-cabiza_grey hover:font-semibold hover:text-primary mr-5'>
-                    Home
-                  </a>
-                </Link>
-              </li>
-              <li className='flex items-center'>
-                <Link href='/discover'>
-                  <a className='px-3 py-4 lg:py-2 flex items-center text-base  text-cabiza_grey hover:font-semibold hover:text-primary mr-5'>
-                    Discover
-                  </a>
-                </Link>
-              </li>
-
-              <li className='flex items-center'>
-                <Link href='/membership'>
-                  <a className='px-3 py-4 lg:py-2 flex items-center text-base text-cabiza_grey hover:font-semibold hover:text-primary'>
-                    Membership
-                  </a>
-                </Link>
-              </li>
-            </ul>
-
-            <ul className='flex flex-col lg:flex-row list-none lg:ml-auto'>
-              <li className='flex items-center'>
-                <Link href='/auth/login'>
-                  <a
-                    className='font-medium md:px-4 py-2 hover:font-semibold focus:outline-none lg:mr-1 lg:mb-0 ml-3 mb-3'
-                    style={{ transition: 'all .15s ease' }}
-                  >
-                    Login
-                  </a>
-                </Link>
-              </li>
-
-              <li className='flex items-center'>
-                <Link href='/auth/signup'>
-                  <a
-                    className='border text-primary border-primary font-semibold px-4 py-2 rounded-xl shadow hover:shadow-md outline-none focus:outline-none lg:mr-1 lg:mb-0 md:ml-3 mb-3 flex items-center'
-                    style={{ transition: 'all .15s ease' }}
-                  >
-                    Sign up <RightIcon/>
-                  </a>
-                </Link>
-              </li>
-            </ul>
-          </div>
+            <svg className="w-5 text-gray-600" viewBox="0 0 24 24">
+              <path
+                fill="currentColor"
+                d="M23,13H1c-0.6,0-1-0.4-1-1s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,13,23,13z"
+              />
+              <path
+                fill="currentColor"
+                d="M23,6H1C0.4,6,0,5.6,0,5s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,6,23,6z"
+              />
+              <path
+                fill="currentColor"
+                d="M23,20H1c-0.6,0-1-0.4-1-1s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,20,23,20z"
+              />
+            </svg>
+          </button>
+          {isMenuOpen && (
+            <div className="absolute top-0 left-0 w-full">
+              <div className="p-5 bg-white border rounded shadow-sm">
+                <div className="flex items-center justify-between mb-4">
+                  <div>
+                    <a
+                      aria-label="Cabiza"
+                      title="Cabiza"
+                      className="inline-flex items-center"
+                    >
+                      <svg
+                        className="w-8 text-deep-purple-accent-400"
+                        viewBox="0 0 24 24"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        strokeLinecap="round"
+                        strokeMiterlimit="10"
+                        stroke="currentColor"
+                        fill="none"
+                      >
+                        <rect x="3" y="1" width="7" height="12" />
+                        <rect x="3" y="17" width="7" height="6" />
+                        <rect x="14" y="1" width="7" height="6" />
+                        <rect x="14" y="11" width="7" height="12" />
+                      </svg>
+                      <span className="ml-2 text-xl font-bold tracking-wide text-gray-800 uppercase">
+                        Cabiza
+                      </span>
+                    </a>
+                  </div>
+                  <div>
+                    <button
+                      aria-label="Close Menu"
+                      title="Close Menu"
+                      className="p-2 -mt-2 -mr-2 transition duration-200 rounded hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
+                      onClick={() => setIsMenuOpen(false)}
+                    >
+                      <svg className="w-5 text-gray-600" viewBox="0 0 24 24">
+                        <path
+                          fill="currentColor"
+                          d="M19.7,4.3c-0.4-0.4-1-0.4-1.4,0L12,10.6L5.7,4.3c-0.4-0.4-1-0.4-1.4,0s-0.4,1,0,1.4l6.3,6.3l-6.3,6.3 c-0.4,0.4-0.4,1,0,1.4C4.5,19.9,4.7,20,5,20s0.5-0.1,0.7-0.3l6.3-6.3l6.3,6.3c0.2,0.2,0.5,0.3,0.7,0.3s0.5-0.1,0.7-0.3 c0.4-0.4,0.4-1,0-1.4L13.4,12l6.3-6.3C20.1,5.3,20.1,4.7,19.7,4.3z"
+                        />
+                      </svg>
+                    </button>
+                  </div>
+                </div>
+                <nav>
+                  <ul className="space-y-4">
+                    <li>
+                      <a
+                        aria-label="Our product"
+                        title="Our product"
+                        className="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
+                      >
+                        Home
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        aria-label="Our product"
+                        title="Our product"
+                        className="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
+                      >
+                        Discover
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        aria-label="Product pricing"
+                        title="Product pricing"
+                        className="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
+                      >
+                        Membership
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        aria-label="About us"
+                        title="About us"
+                        className="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
+                      >
+                        Login
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        className="inline-flex items-center justify-center w-full h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none"
+                        aria-label="Sign up"
+                        title="Sign up"
+                      >
+                        Sign up
+                      </a>
+                    </li>
+                  </ul>
+                </nav>
+              </div>
+            </div>
+          )}
         </div>
-      </nav>
-    </>
+      </div>
+    </div>
   );
 }
