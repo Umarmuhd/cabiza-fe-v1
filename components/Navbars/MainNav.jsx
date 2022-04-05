260cfc6c5628aa27e6ed05999fdeccb5d88c1b16
umarmuhd <umarfarouqft@gmail.com>
2022-04-05 10:18:58 +0100

    applied new cabiza logo to the homepage nav

--- components/Navbars/MainNav.jsx
+++ components/Navbars/MainNav.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Link from "next/link";
+import Image from "next/image";
 
 const NavIcon = () => (
   <svg
@@ -60,24 +61,13 @@ export default function MainNavigation() {
           title="Cabiza"
           className="inline-flex items-center"
         >
-          <svg
-            className="w-8 text-deep-purple-accent-400"
-            viewBox="0 0 24 24"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeMiterlimit="10"
-            stroke="currentColor"
-            fill="none"
-          >
-            <rect x="3" y="1" width="7" height="12" />
-            <rect x="3" y="17" width="7" height="6" />
-            <rect x="14" y="1" width="7" height="6" />
-            <rect x="14" y="11" width="7" height="12" />
-          </svg>
-          <span className="ml-2 text-xl font-bold tracking-wide text-gray-800 uppercase">
-            Cabiza
-          </span>
+          <Image
+            src="/images/logo.png"
+            alt="..."
+            className="w-32"
+            width={130}
+            height={40}
+          />
         </a>
         <ul className="flex items-center hidden space-x-8 lg:flex">
           <li>
