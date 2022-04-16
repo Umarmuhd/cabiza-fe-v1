fb5ae0ea86f4b343c2dc86899f20744fdf0b1aa6
code-art4 <ojotriumph4@gmail.com>
2022-04-16 20:03:21 +0100

    fixed settings page

--- components/Cards/Card.jsx
+++ components/Cards/Card.jsx
@@ -2,19 +2,23 @@ import React from 'react';
 
 const Card = ({title, children, image}) => {
     return (
-      <div className='bg-white rounded-2xl p-10 w-[80%] mx-auto mt-10 shadow-md mb-5'>
+      <div className='bg-white rounded-2xl p-10 md:w-43/50 mx-auto mt-10 shadow-md mb-5'>
         <div className='flex items-center'>
-          {image ? <img src={image} className="mr-20 h-[100%] w-[25rem] rounded-xl shadow-lg"/> : null}
-            <div className="w-[100%]">
-          {title ? (
+          {image ? (
+            <img
+              src={image}
+              className='mr-20 h-[100%] w-[25rem] rounded-xl shadow-lg'
+            />
+          ) : null}
+          <div className='w-[100%]'>
+            {title ? (
               <h3 className='text-2xl text-primary_brand_light font-semibold mb-5'>
                 {title}
               </h3>
-                ) : null}
-              {children}
-            </div>
+            ) : null}
+            {children}
+          </div>
         </div>
-
       </div>
     );
 }
