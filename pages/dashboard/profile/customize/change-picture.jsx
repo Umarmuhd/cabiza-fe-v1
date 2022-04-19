77ae643aea63d548b4faa6927daa81b3b5c1bf14
code-art4 <ojotriumph4@gmail.com>
2022-04-19 21:37:04 +0100

    added image functionality

--- pages/dashboard/profile/customize/change-picture.jsx
+++ pages/dashboard/profile/customize/change-picture.jsx
@@ -29,39 +29,48 @@ const LeftIcon = () => (
 );
 
 const ChangePicture = () => {
-    return (
-      <div className='bg-white rounded-2xl p-10 px-3 md:w-43/50 mx-auto mt-10 shadow-lg mb-12'>
-        <div className='text-center relative'>
-          <div className='rounded-full bg-secondary_sky_light shadow-lg w-[max-content] p-3 mx-auto flex'>
+  return (
+    <div className='bg-white rounded-2xl p-10 px-3 md:w-43/50 mx-auto mt-10 shadow mb-12'>
+      <div className='text-center relative'>
+        <div className='rounded-full bg-secondary_sky_light shadow-lg w-[max-content] p-2 mx-auto flex'>
+          <label htmlFor='image'>
             <img
               src='/images/profile-placeholder.png'
-              className='w-[12rem] h-[auto] m-auto'
+              className='w-[12rem] h-[auto] m-auto pointer'
             />
-          </div>
-          <div className='w-[90%] mx-auto px-10 mt-12'>
-            <h3 className='text-3xl text-secondary_ink_dark font-semibold mb-3 mt-[-1.5rem]'>
-              Choose your profile image
-            </h3>
-            <p className='w-[80%] mx-auto text-secondary_ink_lighter text-lg'>
-              Upload image or drag image to the avatar to upload. Recommended
-              size 100px by 100px, 72 DPI, JPEG or PNG.
-            </p>
+          </label>
+          <input
+            type='file'
+            name='image'
+            accept='image/png, image/gif, image/jpeg'
+            id='image'
+            className="hidden"
+          />
+        </div>
+        <div className='w-[90%] mx-auto px-10 mt-12'>
+          <h3 className='text-3xl text-secondary_ink_dark font-semibold mb-3 mt-[-1.5rem]'>
+            Choose your profile image
+          </h3>
+          <p className='w-[80%] mx-auto text-secondary_ink_lighter text-lg'>
+            Upload image or drag image to the avatar to upload. Recommended size
+            100px by 100px, 72 DPI, JPEG or PNG.
+          </p>
 
-            <button className='mt-10 bg-primary_brand_lightest text-primary py-3 px-7 rounded-3xl'>
-              Upload Picture
-            </button>
-          </div>
-          <div className='mt-7 text-center w-[max-content] mx-auto text-primary'>
-            <Link href='/dashboard/profile/customize'>
-              <a className='flex items-center'>
-                <LeftIcon />
-                <span className='ml-2'>Back</span>
-              </a>
-            </Link>
-          </div>
+          <button className='mt-10 bg-primary_brand_lightest text-primary py-3 px-7 rounded-3xl'>
+            Upload Picture
+          </button>
+        </div>
+        <div className='mt-7 text-center w-[max-content] mx-auto text-primary'>
+          <Link href='/dashboard/profile/customize'>
+            <a className='flex items-center'>
+              <LeftIcon />
+              <span className='ml-2'>Back</span>
+            </a>
+          </Link>
         </div>
       </div>
-    );
-}
+    </div>
+  );
+};
 
 export default ChangePicture;
