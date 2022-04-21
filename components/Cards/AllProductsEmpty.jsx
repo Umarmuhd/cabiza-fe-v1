46ee245027e905df4a1e82c8d83c946ae9fb04b5
code-art4 <ojotriumph4@gmail.com>
2022-04-21 12:54:34 +0100

    added user bio to product

--- components/Cards/AllProductsEmpty.jsx
+++ components/Cards/AllProductsEmpty.jsx
@@ -1,83 +1,122 @@
-import Image from "next/image";
-import React from "react";
+import Image from 'next/image';
+import React from 'react';
+import Link from 'next/link';
 
 const PlusIcon = () => (
   <svg
-    width="52"
-    height="52"
-    viewBox="0 0 52 52"
-    fill="none"
-    xmlns="http://www.w3.org/2000/svg"
+    width='52'
+    height='52'
+    viewBox='0 0 52 52'
+    fill='none'
+    xmlns='http://www.w3.org/2000/svg'
   >
     <path
-      d="M1 26H51"
-      stroke="#404446"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
+      d='M1 26H51'
+      stroke='#404446'
+      strokeWidth='2'
+      strokeLinecap='round'
+      strokeLinejoin='round'
     />
     <path
-      d="M26 51V1"
-      stroke="#404446"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
+      d='M26 51V1'
+      stroke='#404446'
+      strokeWidth='2'
+      strokeLinecap='round'
+      strokeLinejoin='round'
     />
   </svg>
 );
 
-export default function AllProductsEmpty() {
+const PlusSmIcon = () => (
+  <svg
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M18 12.75H6C5.59 12.75 5.25 12.41 5.25 12C5.25 11.59 5.59 11.25 6 11.25H18C18.41 11.25 18.75 11.59 18.75 12C18.75 12.41 18.41 12.75 18 12.75Z"
+      fill="white"
+    />
+    <path
+      d="M12 18.75C11.59 18.75 11.25 18.41 11.25 18V6C11.25 5.59 11.59 5.25 12 5.25C12.41 5.25 12.75 5.59 12.75 6V18C12.75 18.41 12.41 18.75 12 18.75Z"
+      fill="white"
+    />
+  </svg>
+);
+
+export default function AllProductsEmpty({ user }) {
   return (
-    <div
-      className="w-43/50 mx-auto md:my-10 bg-white rounded-3xl text-center p-10"
-      style={{ boxShadow: "0px 20px 40px rgba(0, 0, 0, 0.06)" }}
-    >
+    <>
+      <div className='w-43/50 mx-auto my-10 flex items-center'>
+        <h1 className='text-3xl font-semibold'>
+          Welcome,{' '}
+          <span className='text-secondary_ink_dark capitalize font-normal'>
+            {user?.username}
+          </span>
+        </h1>
+        <p className='text-secondary_brand_light text-md mt-1'>{user?.bio}</p>
+
+        <Link href='/dashboard/products/create'>
+          <a className='py-2 px-4 bg-primary flex items-center font-medium text-white rounded-[48px] ml-auto'>
+            <PlusSmIcon /> <span className='ml-2'>New Product </span>
+          </a>
+        </Link>
+      </div>
       <div
-        className="grid-cols-3
-              space-y-2
-              lg:space-y-0 lg:grid lg:gap-6 lg:grid-rows-1 mb-8 min-h-[250px]"
+        className='w-43/50 mx-auto md:my-10 bg-white rounded-3xl text-center p-10'
+        style={{ boxShadow: '0px 20px 40px rgba(0, 0, 0, 0.06)' }}
       >
-        <div className="rounded-3xl cursor-pointer border border-primary w-full h-full relative">
-          <Image
-            src="/images/create-product.png"
-            alt="..."
-            layout="fill"
-            className="object-cover"
-            objectFit="contain"
-          />
-        </div>
-        <div className="flex items-center justify-center rounded-3xl cursor-pointer bg-secondary_sky_lighter border border-secondary_sky_dark border-dashed">
-          <div
-            className="bg-white p-5 rounded-2xl"
-            style={{ boxShadow: "0px 20px 40px rgba(0, 0, 0, 0.06)" }}
-          >
-            <PlusIcon />
+        <div
+          className='grid-cols-3
+              space-y-2
+              lg:space-y-0 lg:grid lg:gap-6 lg:grid-rows-1 mb-8 min-h-[250px]'
+        >
+          <div className='rounded-3xl cursor-pointer border border-primary w-full h-full relative'>
+            <Image
+              src='/images/create-product.png'
+              alt='...'
+              layout='fill'
+              className='object-cover'
+              objectFit='contain'
+            />
+          </div>
+          <div className='flex items-center justify-center rounded-3xl cursor-pointer bg-secondary_sky_lighter border border-secondary_sky_dark border-dashed'>
+            <div
+              className='bg-white p-5 rounded-2xl'
+              style={{ boxShadow: '0px 20px 40px rgba(0, 0, 0, 0.06)' }}
+            >
+              <PlusIcon />
+            </div>
+          </div>
+          <div className='flex items-center justify-center rounded-3xl cursor-pointer bg-secondary_sky_lighter border border-secondary_sky_dark border-dashed'>
+            <div
+              className='bg-white p-5 rounded-2xl'
+              style={{ boxShadow: '0px 20px 40px rgba(0, 0, 0, 0.06)' }}
+            >
+              <PlusIcon />
+            </div>
           </div>
         </div>
-        <div className="flex items-center justify-center rounded-3xl cursor-pointer bg-secondary_sky_lighter border border-secondary_sky_dark border-dashed">
-          <div
-            className="bg-white p-5 rounded-2xl"
-            style={{ boxShadow: "0px 20px 40px rgba(0, 0, 0, 0.06)" }}
-          >
-            <PlusIcon />
-          </div>
+
+        <h2 className='md:text-4xl font-semibold text-secondary_ink_dark my-6'>
+          Create your first product
+        </h2>
+
+        <p className='text-lg text-secondary_ink_lighter md:w-2/3 md:mx-auto'>
+          Turn your ideas into products, share and connect with people. It’s
+          okay if you make mistakes, first try is not always perfect.
+        </p>
+
+        <div className='flex justify-center mt-6'>
+          <Link href='/dashboard/products/create'>
+            <a className='py-4 px-8 bg-primary_brand_lightest leading-4 font-medium text-primary rounded-4xl'>
+              <span>Create Product</span>
+            </a>
+          </Link>
         </div>
       </div>
-
-      <h2 className="md:text-4xl font-semibold text-secondary_ink_dark my-6">
-        Create your first product
-      </h2>
-
-      <p className="text-lg text-secondary_ink_lighter md:w-2/3 md:mx-auto">
-        Turn your ideas into products, share and connect with people. It’s okay
-        if you make mistakes, first try is not always perfect.
-      </p>
-
-      <div className="flex justify-center mt-6">
-        <button className="py-4 px-8 bg-primary_brand_lightest leading-4 font-medium text-primary rounded-4xl">
-          <span>Create Product</span>
-        </button>
-      </div>
-    </div>
+    </>
   );
 }
--- pages/dashboard/products/index.jsx
+++ pages/dashboard/products/index.jsx
@@ -1,36 +1,17 @@
-import React, { useState, useContext, useEffect } from "react";
-import { Tab } from "@headlessui/react";
+import React, { useState, useContext, useEffect } from 'react';
+import { Tab } from '@headlessui/react';
 
-import AllProductsEmpty from "@/components/Cards/AllProductsEmpty";
+import AllProductsEmpty from '@/components/Cards/AllProductsEmpty';
 
-import Dashboard from "@/layouts/Dashboard";
-import AllProducts from "@/components/Cards/AllProducts";
-import Affiliated from "@/components/Cards/Affiliated";
-import { classNames } from "../../../libs/helper";
-import Link from "next/link";
-import { API_URL } from "@/config/index";
-import AuthContext from "@/context/AuthContext";
-import axios from "axios";
+import Dashboard from '@/layouts/Dashboard';
+import AllProducts from '@/components/Cards/AllProducts';
+import Affiliated from '@/components/Cards/Affiliated';
+import { classNames } from '../../../libs/helper';
+import Link from 'next/link';
+import { API_URL } from '@/config/index';
+import AuthContext from '@/context/AuthContext';
+import axios from 'axios';
 import FullNav from '@/components/Navbars/DashboardNav/FullNav';
-
-const PlusIcon = () => (
-  <svg
-    width="24"
-    height="24"
-    viewBox="0 0 24 24"
-    fill="none"
-    xmlns="http://www.w3.org/2000/svg"
-  >
-    <path
-      d="M18 12.75H6C5.59 12.75 5.25 12.41 5.25 12C5.25 11.59 5.59 11.25 6 11.25H18C18.41 11.25 18.75 11.59 18.75 12C18.75 12.41 18.41 12.75 18 12.75Z"
-      fill="white"
-    />
-    <path
-      d="M12 18.75C11.59 18.75 11.25 18.41 11.25 18V6C11.25 5.59 11.59 5.25 12 5.25C12.41 5.25 12.75 5.59 12.75 6V18C12.75 18.41 12.41 18.75 12 18.75Z"
-      fill="white"
-    />
-  </svg>
-);
 
 const Products = ({}) => {
   const [products, setProducts] = useState([]);
@@ -67,22 +48,47 @@ const Products = ({}) => {
     }
   };
 
+  const SearchIcon = () => (
+    <svg
+      width='16'
+      height='16'
+      viewBox='0 0 16 16'
+      fill='none'
+      xmlns='http://www.w3.org/2000/svg'
+    >
+      <path
+        d='M7.33334 13.3333C10.647 13.3333 13.3333 10.647 13.3333 7.33333C13.3333 4.01962 10.647 1.33333 7.33334 1.33333C4.01963 1.33333 1.33334 4.01962 1.33334 7.33333C1.33334 10.647 4.01963 13.3333 7.33334 13.3333Z'
+        stroke='#303437'
+        strokeWidth='1.5'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+      />
+      <path
+        d='M12.62 13.7932C12.9733 14.8599 13.78 14.9665 14.4 14.0332C14.9666 13.1799 14.5933 12.4799 13.5666 12.4799C12.8066 12.4732 12.38 13.0665 12.62 13.7932Z'
+        stroke='#303437'
+        strokeWidth='1.5'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+      />
+    </svg>
+  );
+
   useEffect(() => fetchBalance(), []);
 
   return (
     <div>
-      <Tab.Group>        
-      <FullNav title='Products'>
-          <div className='flex justify-between items-center md:w-43/50 mx-auto'>
-            <Tab.List as={'div'} className='flex items-center'>
+      <Tab.Group>
+        <FullNav title='Products'>
+          <div className='flex justify-between items-center md:w-43/50 mx-auto pr-4'>
+            <Tab.List as={'div'} className='flex items-center py-[.5rem]'>
               <Tab
                 as={'button'}
                 className={({ selected }) =>
                   classNames(
-                    'text-lg mr-10',
+                    'text-md mr-10 pb-2 font-medium',
                     selected
-                      ? ' font-bold text-primary'
-                      : ' font-medium text-cabiza_grey'
+                      ? ' font-bold text-primary border-b border-b-primary'
+                      : ' text-secondary'
                   )
                 }
               >
@@ -92,10 +98,10 @@ const Products = ({}) => {
                 as={'button'}
                 className={({ selected }) =>
                   classNames(
-                    'text-lg mr-10',
+                    'text-md mr-10 pb-2 font-medium',
                     selected
-                      ? ' font-bold text-primary'
-                      : ' font-medium text-cabiza_grey'
+                      ? ' font-bold text-primary border-b border-b-primary'
+                      : ' text-secondary'
                   )
                 }
               >
@@ -103,11 +109,14 @@ const Products = ({}) => {
               </Tab>
             </Tab.List>
 
-            <div className='flex items-center justify-between'>
-              <div className='flex rounded-[100px] shadow-sm mr-6'>
+            <div className='flex items-center justify-between mt-[-.5rem]'>
+              <button className='flex items-center justify-center bg-primary_brand_lightest hover:bg-slate-200 transition duration-150 p-2 rounded-full'>
+                <SearchIcon />
+              </button>
+              {/* <div className='flex rounded-[100px] shadow-sm mr-6'>
                 <input
                   type='text'
-                  className='border border-r-0 border-grey_80 px-4 py-2 placeholder-grey_80 text-grey_40 bg-white shadow-sm focus:outline-none focus:ring w-full rounded-l-[100px]'
+                  className='border border-r-0 border-grey_80 px-4 py-2 placeholder-grey_80 text-grey_40 bg-white shadow-sm focus:outline-none w-full rounded-l-[100px]'
                   style={{ transition: 'all 0.15s ease 0s' }}
                   id='search'
                   placeholder='Search...'
@@ -122,18 +131,17 @@ const Products = ({}) => {
                 <a className='py-2 px-4 bg-primary flex items-center font-medium text-white rounded-[48px]'>
                   <PlusIcon /> <span className='ml-2'>New Product </span>
                 </a>
-              </Link>
+              </Link> */}
             </div>
           </div>
         </FullNav>
-
 
         <Tab.Panels>
           <Tab.Panel>
             {products.length > 0 ? (
               <AllProducts products={products} balance={balance} />
             ) : (
-              <AllProductsEmpty />
+              <AllProductsEmpty user={user} />
             )}
           </Tab.Panel>
           <Tab.Panel>
