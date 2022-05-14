474b6acaa7234e89ee593e8a692f2ae90838371a
umarmuhd <umarfarouqft@gmail.com>
2022-05-14 21:09:10 +0100

    added tawkto for support

--- pages/dashboard/help/index.jsx
+++ pages/dashboard/help/index.jsx
@@ -1,8 +1,25 @@
-import React from "react";
-import Dashboard from "@/layouts/Dashboard";
+import React, { useEffect } from 'react';
+import Dashboard from '@/layouts/Dashboard';
+
+import TawkTo from 'tawkto-react';
 
 export default function Help() {
-  return <div>Help page</div>;
+  const propertyId = process.env.NEXT_PUBLIC_PROPERTY_ID;
+  const widgetId = process.env.NEXT_PUBLIC_WIDGET_ID;
+
+  useEffect(() => {
+    var tawk = new TawkTo(propertyId, widgetId);
+
+    tawk.onStatusChange((status) => {
+      console.log(status);
+    });
+  }, []);
+
+  return (
+    <div>
+      <p>Help page</p>{' '}
+    </div>
+  );
 }
 
 Help.layout = Dashboard;
