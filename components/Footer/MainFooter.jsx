c107b621b92573ccc5f1ee83a7196cb7342a7cd8
code-art4 <ojotriumph4@gmail.com>
2022-05-25 14:03:02 +0100

    fixed home page

--- components/Footer/MainFooter.jsx
+++ components/Footer/MainFooter.jsx
@@ -5,9 +5,9 @@ export default function MainFooter() {
   return (
     <footer className="bg-footer text-white pt-3">
       <div className="container mx-auto mt-8 px-8 flex">
-        <div className="w-full flex flex-col lg:flex-row md:flex-column py-6">
+        <div className="w-full flex flex-col md:flex-row md:flex-column py-6">
           {/* md:w-2/5 */}
-          <div className="mb-6 mr-auto">
+          <div className="mb-6 mr-auto w-[60%]">
             <Image
             src="/images/footer-logo.svg"
             alt="..."
@@ -15,13 +15,13 @@ export default function MainFooter() {
             width={300}
             height={90}
           />
-            <p className="md:w-2/4 font-medium text-xl mt-12">
+            <p className="md:w-3/4 font-medium text-xl mt-12">
               2120 Broadway, Suite 112 Los Angeles, CA 12345
             </p>
           </div>
 
           {/* md:w-3/5 */}
-          <div className="flex flex-col md:flex-row w-[20rem]">
+          <div className="flex md:flex-col sm:mt-10 flex-row w-[20rem]">
             <div className="flex-1 mr-5">
               <p className="font-extrabold text-primary md:mb-4">
                 Others
