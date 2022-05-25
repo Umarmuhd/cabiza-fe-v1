7f078040e54fe608aac6a1d725d1d22f620db045
umarmuhd <umarfarouqft@gmail.com>
2022-05-25 11:24:32 +0100

    sidebar fix

--- pages/auth/login/index.jsx
+++ pages/auth/login/index.jsx
@@ -1,14 +1,15 @@
-import React, { useState, useContext } from "react";
-import Link from "next/link";
-import { useForm } from "react-hook-form";
-import axios from "axios";
-import { useRouter } from "next/router";
+import React, { useState, useContext } from 'react';
+import Link from 'next/link';
+import { useForm } from 'react-hook-form';
+import axios from 'axios';
+import { useRouter } from 'next/router';
 
-import AuthContext from "@/context/AuthContext";
+import AuthContext from '@/context/AuthContext';
 
-import Auth from "layouts/Auth";
-import { NEXT_URL } from "config/index";
-import toast from "react-hot-toast";
+import Auth from 'layouts/Auth';
+import { NEXT_URL } from 'config/index';
+import toast from 'react-hot-toast';
+import Alert from '@/components/Alert';
 
 export default function Login() {
   const {
@@ -19,7 +20,7 @@ export default function Login() {
 
   const { loginUser } = useContext(AuthContext);
   const [loading, setLoading] = useState(false);
-  const [error, setError] = useState("");
+  const [error, setError] = useState('');
 
   const router = useRouter();
 
@@ -31,32 +32,14 @@ export default function Login() {
 
       const { user, token } = response.data;
 
-      toast.custom(
-        <div className="rounded-lg py-4 px-8 bg-[#24C78C] flex items-center">
-          <svg
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M16.19 2H7.81C4.17 2 2 4.17 2 7.81V16.18C2 19.83 4.17 22 7.81 22H16.18C19.82 22 21.99 19.83 21.99 16.19V7.81C22 4.17 19.83 2 16.19 2ZM15.36 14.3C15.65 14.59 15.65 15.07 15.36 15.36C15.21 15.51 15.02 15.58 14.83 15.58C14.64 15.58 14.45 15.51 14.3 15.36L12 13.06L9.7 15.36C9.55 15.51 9.36 15.58 9.17 15.58C8.98 15.58 8.79 15.51 8.64 15.36C8.35 15.07 8.35 14.59 8.64 14.3L10.94 12L8.64 9.7C8.35 9.41 8.35 8.93 8.64 8.64C8.93 8.35 9.41 8.35 9.7 8.64L12 10.94L14.3 8.64C14.59 8.35 15.07 8.35 15.36 8.64C15.65 8.93 15.65 9.41 15.36 9.7L13.06 12L15.36 14.3Z"
-              fill="white"
-            />
-          </svg>
-          <span className="ml-2.5 font-medium text-lg text-white">
-            Login success !
-          </span>
-        </div>
-      );
+      toast.custom(<Alert color="#24C78C" text="Login successful !" />);
 
       loginUser(user, token);
       setLoading(false);
 
-      const status = user.username ? "profile" : "onboarding";
+      const status = user.username ? 'profile' : 'onboarding';
 
-      return router.push("/dashboard/" + status);
+      return router.push('/dashboard/' + status);
     } catch (error) {
       setError(error.response.data.message);
       setLoading(false);
@@ -80,12 +63,12 @@ export default function Login() {
               <input
                 type="email"
                 className="border border-grey_80 px-4 py-3 placeholder-grey_80 text-grey_40 bg-white shadow-sm focus:outline-none focus:ring w-full rounded-lg"
-                style={{ transition: "all 0.15s ease 0s" }}
+                style={{ transition: 'all 0.15s ease 0s' }}
                 id="email"
                 placeholder="e.g cabizahere@gmail.com"
-                {...register("email", { required: true })}
+                {...register('email', { required: true })}
               />
-              {errors.email?.type === "required" && (
+              {errors.email?.type === 'required' && (
                 <p className="text-left text-red-600 text-xs mt-1">
                   Email address is required
                 </p>
@@ -101,10 +84,10 @@ export default function Login() {
               <input
                 type="password"
                 className="border border-grey_80 px-4 py-3 placeholder-grey_80 text-grey_40 bg-white shadow-sm focus:outline-none focus:ring w-full rounded-lg"
-                style={{ transition: "all 0.15s ease 0s" }}
+                style={{ transition: 'all 0.15s ease 0s' }}
                 id="password"
                 placeholder="........"
-                {...register("password", { required: true, minLength: 6 })}
+                {...register('password', { required: true, minLength: 6 })}
               />
               {errors?.password ? (
                 <p className="text-left text-red-600 text-xs mt-1">
@@ -114,7 +97,7 @@ export default function Login() {
             </div>
             <div className="relative flex justify-end">
               <p className="text-lg text-right text-grey_40">
-                Forgot password?{" "}
+                Forgot password?{' '}
                 <Link href="/auth/forget-password">
                   <a className="ml-1 underline cursor-pointer">Reset it</a>
                 </Link>
@@ -125,16 +108,16 @@ export default function Login() {
                 className="bg-primary text-white active:bg-primary text-lg font-semibold px-6 py-3 rounded-lg outline-none focus:outline-none w-full"
                 type="submit"
                 style={{
-                  transition: "all 0.15s ease 0s",
-                  boxShadow: "0px 8px 30px rgba(0, 0, 0, 0.08)",
+                  transition: 'all 0.15s ease 0s',
+                  boxShadow: '0px 8px 30px rgba(0, 0, 0, 0.08)',
                 }}
                 disabled={loading}
               >
-                {loading ? "..." : "Login"}
+                {loading ? '...' : 'Login'}
               </button>
             </div>
             <p className="text-center text-grey_40 text-lg">
-              Already have an account?{" "}
+              Don't already have an account
               <Link href="/auth/signup">
                 <a className="text-primary ml-1">Signup</a>
               </Link>
--- pages/dashboard/onboarding/index.jsx
+++ pages/dashboard/onboarding/index.jsx
@@ -1,15 +1,15 @@
-import React, { useState, useContext } from "react";
-import { useForm } from "react-hook-form";
-import { Switch } from "@headlessui/react";
-import axios from "axios";
-import { API_URL } from "config/index";
-import { useRouter } from "next/router";
-import Image from "next/image";
+import React, { useState, useContext } from 'react';
+import { useForm } from 'react-hook-form';
+import { Switch } from '@headlessui/react';
+import axios from 'axios';
+import { API_URL } from 'config/index';
+import { useRouter } from 'next/router';
+import Image from 'next/image';
 
-import { countries } from "libs/CountriesList";
+import { countries } from 'libs/CountriesList';
 
-import AuthContext from "@/context/AuthContext";
-import toast from "react-hot-toast";
+import AuthContext from '@/context/AuthContext';
+import toast from 'react-hot-toast';
 
 export default function Onboarding() {
   const CategorySelect = React.forwardRef(
@@ -25,7 +25,7 @@ export default function Onboarding() {
           defaultValue={defaultValue}
         >
           <option />
-          {["Education", "Fitness", "Health"].map((category) => (
+          {['Education', 'Fitness', 'Health'].map((category) => (
             <option key={category} value={category}>
               {category}
             </option>
@@ -45,7 +45,7 @@ export default function Onboarding() {
       </>
     )
   );
-  CategorySelect.displayName = "CategorySelect";
+  CategorySelect.displayName = 'CategorySelect';
   const CountrySelect = React.forwardRef(
     ({ onChange, onBlur, name, label, defaultValue }, ref) => (
       <>
@@ -79,7 +79,7 @@ export default function Onboarding() {
       </>
     )
   );
-  CountrySelect.displayName = "CountrySelect";
+  CountrySelect.displayName = 'CountrySelect';
 
   const [enabled, setEnabled] = React.useState(false);
   const [loading, setLoading] = useState(false);
@@ -101,11 +101,11 @@ export default function Onboarding() {
 
       const form_data = new FormData();
 
-      form_data.append("username", username);
-      form_data.append("photo", photo[0]);
-      form_data.append("bio", bio);
-      form_data.append("country", country);
-      form_data.append("category", category);
+      form_data.append('username', username);
+      form_data.append('photo', photo[0]);
+      form_data.append('bio', bio);
+      form_data.append('country', country);
+      form_data.append('category', category);
 
       const url = `${API_URL}/user/onboarding`;
 
@@ -113,7 +113,7 @@ export default function Onboarding() {
 
       toast.success(response.data.message);
       setLoading(false);
-      router.replace("/dashboard/posts");
+      router.replace('/dashboard/profile');
     } catch (error) {
       console.log(error);
       setLoading(false);
@@ -158,10 +158,10 @@ export default function Onboarding() {
               <input
                 type="text"
                 className="border border-r-0 border-secondary_sky_base px-4 py-3 placeholder-grey_80 text-grey_40 bg-white focus:outline-none focus:ring w-full rounded-l-lg"
-                style={{ transition: "all 0.15s ease 0s" }}
+                style={{ transition: 'all 0.15s ease 0s' }}
                 id="username"
                 placeholder="jake"
-                {...register("username", { required: true })}
+                {...register('username', { required: true })}
               />
               <div className="p-2 border border-secondary_sky_base border-l-0 rounded-r-lg">
                 <span className="flex p-2 rounded-lg border border-primary_brand_light bg-primary_brand_lightest text-primary_brand_light text-base leading-4">
@@ -181,7 +181,7 @@ export default function Onboarding() {
               className="form-control rounded-lg block w-full px-2 py-3 text-base font-normal text-secondary_sky_dark bg-white bg-clip-padding border border-solid border-secondary_sky_base transition ease-in-out m-0 focus:text-grey_80 focus:bg-white focus:border-blue-600 focus:outline-none leading-4"
               type="file"
               id="formFile"
-              {...register("photo", { required: true })}
+              {...register('photo', { required: true })}
             />
 
             <style jsx>{`           
@@ -209,10 +209,10 @@ export default function Onboarding() {
             <input
               type="text"
               className="border border-secondary_sky_base px-4 py-3 placeholder-grey_80 text-grey_40 bg-white focus:outline-none focus:ring w-full rounded-lg"
-              style={{ transition: "all 0.15s ease 0s" }}
+              style={{ transition: 'all 0.15s ease 0s' }}
               id="name"
               placeholder="Pseudonym, organization name or full name"
-              {...register("name", { required: true })}
+              {...register('name', { required: true })}
               defaultValue={user?.full_name}
             />
           </div>
@@ -227,19 +227,19 @@ export default function Onboarding() {
             <input
               type="text"
               className="border border-secondary_sky_base px-4 py-3 placeholder-grey_80 text-grey_40 bg-white focus:outline-none focus:ring w-full rounded-lg"
-              style={{ transition: "all 0.15s ease 0s" }}
+              style={{ transition: 'all 0.15s ease 0s' }}
               id="bio"
               placeholder="Tell us about yourself and what you do"
-              {...register("bio", { required: true })}
+              {...register('bio', { required: true })}
             />
           </div>
 
           <div className="relative mb-6">
-            <CategorySelect label="Category" {...register("category")} />
+            <CategorySelect label="Category" {...register('category')} />
           </div>
 
           <div className="relative mb-6">
-            <CountrySelect label="Country" {...register("country")} />
+            <CountrySelect label="Country" {...register('country')} />
             <small className="text-grey_40 text-sm">
               You will receive payouts in USD directly into your PayPal account.
             </small>
@@ -250,19 +250,19 @@ export default function Onboarding() {
               checked={enabled}
               onChange={setEnabled}
               className={
-                (enabled ? "bg-primary_brand_base" : "bg-secondary_sky_light") +
-                " relative inline-flex flex-shrink-0 h-[18px] w-[32px] border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus-visible:ring-2  focus-visible:ring-white focus-visible:ring-opacity-75"
+                (enabled ? 'bg-primary_brand_base' : 'bg-secondary_sky_light') +
+                ' relative inline-flex flex-shrink-0 h-[18px] w-[32px] border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus-visible:ring-2  focus-visible:ring-white focus-visible:ring-opacity-75'
               }
             >
               <span className="sr-only">Use setting</span>
               <span
                 aria-hidden="true"
-                className={`${enabled ? "translate-x-4" : "translate-x-0"}
+                className={`${enabled ? 'translate-x-4' : 'translate-x-0'}
 pointer-events-none inline-block h-[14px] w-[13px] rounded-full bg-white shadow-lg transform ring-0 transition ease-in-out duration-200`}
               />
             </Switch>
             <span className="ml-2 block text-base text-grey_20">
-              {"Send me updates on new features & tips for creators"}
+              {'Send me updates on new features & tips for creators'}
             </span>
           </div>
 
@@ -271,7 +271,7 @@ pointer-events-none inline-block h-[14px] w-[13px] rounded-full bg-white shadow-
             type="submit"
             disabled={loading}
           >
-            {loading ? "..." : "Update Profile"}
+            {loading ? '...' : 'Update Profile'}
           </button>
         </form>
       </div>
