f7bcef518f39c54612e3adef2ae5754acae17047
code-art4 <ojotriumph4@gmail.com>
2022-06-04 13:24:01 +0100

    fixed issues

--- pages/dashboard/products/index.jsx
+++ pages/dashboard/products/index.jsx
@@ -113,7 +113,7 @@ const Products = ({}) => {
   } 
 
   useEffect(() => {
-    posts.includes === "" ? setProducts(products.filter(product => product.name.includes(router.query.s))) : setProducts(products)
+    products.includes === "" ? setProducts(products.filter(product => product.name.includes(router.query.s))) : setProducts(products)
   }, [router.query.s])
 
   return (
