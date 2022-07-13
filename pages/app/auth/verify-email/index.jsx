e631d1fd091221b2d44eb1fae2b862c9af394a94
umarmuhd <umarfarouqft@gmail.com>
2022-07-13 09:32:29 +0100

    Auth layout fixes

--- pages/app/auth/verify-email/index.jsx
+++ pages/app/auth/verify-email/index.jsx
@@ -16,12 +16,12 @@ const SpinIcon = () => (
       cx="12"
       cy="12"
       r="10"
-      stroke="#006156"
+      stroke="#5B44E9"
       strokeWidth="4"
     ></circle>
     <path
       className="opacity-75"
-      fill="#006156"
+      fill="#5B44E9"
       d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
     ></path>
   </svg>
@@ -65,7 +65,7 @@ export default function VerifyEmail() {
   return (
     <div className="bg-light min-h-screen grid place-content-center">
       <div className="max-w-2xl mx-auto">
-        <h1 className="text-lg md:text-xl font-semibold text-grey_40">
+        <h1 className="text-lg md:text-xl font-medium text-grey_40">
           {loading ? <SpinIcon /> : "Verifying email. Please wait ..."}
         </h1>
       </div>
