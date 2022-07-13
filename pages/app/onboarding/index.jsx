b63fc80251cb5de3c7544ad7a415dde281bf8ca5
umarmuhd <umarfarouqft@gmail.com>
2022-07-13 09:24:09 +0100

    Fix to onboarding layout

--- pages/app/onboarding/index.jsx
+++ pages/app/onboarding/index.jsx
@@ -120,7 +120,7 @@ export default function Onboarding() {
   return (
     <div className="flex flex-wrap w-full min-h-screen h-full">
       <div className="w-1/2 shadow-2xl bg-onboarding-pattern bg-contain inset-0 min-h-max hidden md:flex items-center">
-        <div className="px-4 md:px-0 md:max-w-[80%] mx-auto">
+        <div className="md:max-w-[80%] mx-auto">
           <div className="text-white text-center mb-20">
             <h1 className="font-bold text-5xl mb-10">
               Welcome back, <br /> Set up your profile
@@ -134,7 +134,7 @@ export default function Onboarding() {
           <img src="/images/onboarding-illustration.svg" alt="..." />
         </div>
       </div>
-      <div className="flex flex-col w-full md:w-1/2 p-10">
+      <div className="flex flex-col w-full md:w-1/2 p-10 px-4 md:px-10">
         <div className="w-[40%] mr-auto h-[auto]">
           <img
             src="/images/logo.png"
